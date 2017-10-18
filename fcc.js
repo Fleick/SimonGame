9fdd94ed3ae0c752b2b5a20323b4eadd83257b9a
Florian Eickenbusch <florian.eickenbusch@sharerace.com>
2017-10-18 22:52:08 +0200

    Working Counter

--- fcc.js
+++ fcc.js
@@ -1,16 +1,25 @@
 const store = new Vuex.Store({
     state: {
-        count: 0
+        count: 0,
+        numbers: [0,0,0]
     },
     mutations: {
         changeCount(state, newCount){
             state.count = newCount;
-            console.log(state.count);
+            console.log('store.changeCount: ' + newCount);
+        },
+        increaseCount(state){
+          state.count++;
+          console.log('store.increaseCount: ' + state.count);
+        },
+        changeNumbers(state, newNumbers){
+          state.numbers = newNumbers;
+          console.log('store.changeNumbers: ' + state.numbers);
         }
     }
 })
 
-var number = Vue.component('number', {
+const number = Vue.component('number', {
    template: '<div class="number"> ' +
                 '<div class="top" ' +
                      ':class="{on: topOn}"></div>' +
@@ -30,27 +39,124 @@ var number = Vue.component('number', {
     data: function () {
        return {
            /* Number parts on or off */
-           topOn: false,
-           topLeftOn: false,
+           topOn: true,
+           topLeftOn: true,
            topRightOn: true,
            middleOn: false,
-           bottomLeftOn: false,
+           bottomLeftOn: true,
            bottomRightOn: true,
-           bottomOn: false
+           bottomOn: true,
        }
     },
-    methods: {
-       changeNumber: function () {
-           console.log("change Number");
-       }
+    props: ['pos'],
+    computed: {
+      number(){return store.state.numbers[this.pos];}
+    },
+    watch: {
+      number(newValue){
+        switch(newValue){
+          case 0:
+            this.topOn = true;
+            this.topLeftOn = true;
+            this.topRightOn = true;
+            this.middleOn = false;
+            this.bottomLeftOn = true;
+            this.bottomRightOn = true;
+            this.bottomOn = true;
+            break;
+          case 1:
+            this.topOn = false;
+            this.topLeftOn = false;
+            this.topRightOn = true;
+            this.middleOn = false;
+            this.bottomLeftOn = false;
+            this.bottomRightOn = true;
+            this.bottomOn = false;
+            break;
+          case 2:
+            this.topOn = true;
+            this.topLeftOn = false;
+            this.topRightOn = true;
+            this.middleOn = true;
+            this.bottomLeftOn = true;
+            this.bottomRightOn = false;
+            this.bottomOn = true;
+            break;
+          case 3:
+            this.topOn = true;
+            this.topLeftOn = false;
+            this.topRightOn = true;
+            this.middleOn = true;
+            this.bottomLeftOn = false;
+            this.bottomRightOn = true;
+            this.bottomOn = true;
+            break;
+          case 4:
+            this.topOn = false;
+            this.topLeftOn = true;
+            this.topRightOn = true;
+            this.middleOn = true;
+            this.bottomLeftOn = false;
+            this.bottomRightOn = true;
+            this.bottomOn = false;
+            break;
+          case 5:
+            this.topOn = true;
+            this.topLeftOn = true;
+            this.topRightOn = false;
+            this.middleOn = true;
+            this.bottomLeftOn = false;
+            this.bottomRightOn = true;
+            this.bottomOn = true;
+            break;
+          case 6:
+            this.topOn = true;
+            this.topLeftOn = true;
+            this.topRightOn = false;
+            this.middleOn = true;
+            this.bottomLeftOn = true;
+            this.bottomRightOn = true;
+            this.bottomOn = true;
+            break;
+          case 7:
+            this.topOn = true;
+            this.topLeftOn = false;
+            this.topRightOn = true;
+            this.middleOn = false;
+            this.bottomLeftOn = false;
+            this.bottomRightOn = true;
+            this.bottomOn = false;
+            break;
+          case 8:
+            this.topOn = true;
+            this.topLeftOn = true;
+            this.topRightOn = true;
+            this.middleOn = true;
+            this.bottomLeftOn = true;
+            this.bottomRightOn = true;
+            this.bottomOn = true;
+            break;
+          case 9:
+            this.topOn = true;
+            this.topLeftOn = true;
+            this.topRightOn = true;
+            this.middleOn = true;
+            this.bottomLeftOn = false;
+            this.bottomRightOn = true;
+            this.bottomOn = true;
+            break;
+          default:
+            alert("No Display defined for: " + newValue);
+        }
+      }
     }
+
 });
-var counter = Vue.component('counter', {
+const counter = Vue.component('counter', {
    template: '<div id="counter">' +
-                '<number ref="number1" style="float: left"></number>' +
-                '<number ref="number2"></number>' +
-                '<number ref="number3" style="float:right"></number>' +
-       '<button @click="state">Test</button>' +
+                '<number :pos="0" style="float: left"></number>' +
+                '<number :pos="1"></number>' +
+                '<number :pos="2" style="float:right"></number>' +
             '</div>',
     components: {
        number: number
@@ -65,14 +171,24 @@ var counter = Vue.component('counter', {
     methods: {
        changeCounter: function () {
            this.number1.changeNumber();
-       },
-       state: function () {
-           store.commit('changeCount', 20);
        }
+    },
+    computed: {
+      count() {return store.state.count;}
+    },
+    watch: {
+      count(newValue){
+        var numbers = [
+          Math.floor(newValue / 100),
+          Math.floor(newValue /10) % 10,
+          newValue % 10
+        ];
+        store.commit('changeNumbers', numbers);
+      }
     }
 });
 
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
       running: false,
@@ -82,24 +198,31 @@ var app = new Vue({
       counter: counter
   },
   methods: {
+      state: function () {
+        store.commit('increaseCount');
+      },
+      stateTwo: function() {
+        var count = window.prompt("Inser new count", 0);;
+        store.commit('changeCount', count);
+      },
       TopLeft: function () {
-        
+
       },
       TopRight: function () {
-          
+
       },
       BottomLeft: function () {
-          
+
       },
       BottomRight: function () {
-          
+
       },
       Start: function () {
           this.topLeftOn = !this.topLeftOn;
           console.log(this.topLeftOn);
       },
       Strict: function () {
-          
+
       },
       SetCounter: function (number) {
 
